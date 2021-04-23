acf61865d88733b82e094108f20572baadad67de
euguroglu <e.uguroglu@yahoo.com>
2021-04-24 01:01:27 +0300

    subdag added

--- Tutorial_2/parallel_dag_with_subdag.py
+++ Tutorial_2/parallel_dag_with_subdag.py
@@ -1,5 +1,7 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+from airflow.operators.subdag import SubDagOperator
+from subdags.subdag_parallel_dag import subdag_parallel_dag
 
 from datetime import datetime
 
@@ -15,14 +17,9 @@ with DAG('parallel_dag',schedule_interval='@daily',
             bash_command = 'sleep 3'
         )
 
-        bash_operator_2 = BashOperator(
-            task_id = "bash_operator_2",
-            bash_command = 'sleep 3'
-        )
-
-        bash_operator_3 = BashOperator(
-            task_id = "bash_operator_3",
-            bash_command = 'sleep 3'
+        processing = SubDagOperator(
+            task_id = "processing_tasks",
+            subdag = subdag_parallel_dag('parallel_dag', 'processing_tasks', default_args)
         )
 
         bash_operator_4 = BashOperator(
@@ -30,4 +27,4 @@ with DAG('parallel_dag',schedule_interval='@daily',
             bash_command = 'sleep 3'
         )
 
-        bash_operator_1 << [bash_operator_2,bash_operator_3] << bash_operator_4
+        bash_operator_1 << processing << bash_operator_4
