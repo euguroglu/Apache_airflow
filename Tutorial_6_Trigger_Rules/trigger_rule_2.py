b000833656cdaa6c3c7b6e6d8815aa095c121789
euguroglu <e.uguroglu@yahoo.com>
2021-04-25 01:25:44 +0300

    trigger rule all failed added

--- Tutorial_6_Trigger_Rules/trigger_rule_2.py
+++ Tutorial_6_Trigger_Rules/trigger_rule_2.py
@@ -11,20 +11,21 @@ with DAG('trigger_rule_2', schedule_interval='@daily', default_args=default_args
 
     task_1 = BashOperator(
         task_id = 'task_1',
-        bash_command = 'exit 0',
+        bash_command = 'exit 1',
         do_xcom_push=False
     )
 
     task_2 = BashOperator(
         task_id = 'task_2',
-        bash_command = 'exit 0',
+        bash_command = 'exit 1',
         do_xcom_push=False
     )
 
     task_3 = BashOperator(
         task_id = 'task_3',
         bash_command = 'exit 0',
-        do_xcom_push=False
+        do_xcom_push=False,
+        trigger_rule ='all_failed'
     )
 
     [task_1, task_2] >> task_3
