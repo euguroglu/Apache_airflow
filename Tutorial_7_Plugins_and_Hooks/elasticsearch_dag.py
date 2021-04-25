541bb9246deeb1cef07c13eef844072faaad9622
euguroglu <e.uguroglu@yahoo.com>
2021-04-25 16:15:19 +0300

    postgres to elasticsearch operators added

--- Tutorial_7_Plugins_and_Hooks/elasticsearch_dag.py
+++ Tutorial_7_Plugins_and_Hooks/elasticsearch_dag.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from elasticsearch_plugin.hooks.elastic_hook import ElasticHook
+from elasticsearch_plugin.operators.postgres_to_elastic import PostgresToElasticOperator
 from airflow.operators.python import PythonOperator
 
 from datetime import datetime
@@ -19,3 +20,11 @@ with DAG('elasticsearch_dag', schedule_interval='@daily',
         task_id = 'print_es_info',
         python_callable = _print_es_info
     )
+
+    connections_to_es = PostgresToElasticOperator(
+        task_id = 'connections_to_es',
+        sql = 'SELECT * FROM connection',
+        index = 'connections'
+    )
+
+    print_es_info >> connections_to_es
