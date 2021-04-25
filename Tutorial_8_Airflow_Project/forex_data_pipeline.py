aad6bf5a7da5e8caa1c49f5fb8c07e1dc4c9d305
euguroglu <e.uguroglu@yahoo.com>
2021-04-25 23:26:27 +0300

    python operator example added

--- Tutorial_8_Airflow_Project/forex_data_pipeline.py
+++ Tutorial_8_Airflow_Project/forex_data_pipeline.py
@@ -1,8 +1,12 @@
 from airflow import DAG
 from airflow.providers.http.sensors.http import HttpSensor
 from airflow.sensors.filesystem import FileSensor
+from airflow.operators.python import PythonOperator
 
 from datetime import datetime, timedelta
+import csv
+import requests
+import json
 
 default_args = {
     "owner": "airflow",
@@ -12,6 +16,25 @@ default_args = {
     "retry": 1,
     "retry_delay": timedelta(minutes=5)
 }
+
+def download_rates():
+    BASE_URL = "https://gist.githubusercontent.com/marclamberti/f45f872dea4dfd3eaa015a4a1af4b39b/raw/"
+    ENDPOINTS = {
+        'USD': 'api_forex_exchange_usd.json',
+        'EUR': 'api_forex_exchange_eur.json'
+    }
+    with open('/opt/airflow/dags/files/forex_currencies.csv') as forex_currencies:
+        reader = csv.DictReader(forex_currencies, delimiter=',')
+        for idx, row in enumerate(reader):
+            base = row['base']
+            with_pairs = row['with_pairs'].split(' ')
+            indata = requests.get(f"{BASE_URL}{ENDPOINTS[base]}").json()
+            outdata = {'base': base, 'rates': {}, 'last_update': indata['date']}
+            for pair in with_pairs:
+                outdata['rates'][pair] = indata['rates'][pair]
+            with open('/home/enes/airflow2/dags/files/forex_rates.json', 'a') as outfile:
+                json.dump(outdata, outfile)
+                outfile.write('\n')
 
 with DAG("forex_data_pipeline",start_date=datetime(2021, 1, 1),
          schedule_interval="@daily", default_args=default_args, catchup=False) as dag:
@@ -33,4 +56,10 @@ with DAG("forex_data_pipeline",start_date=datetime(2021, 1, 1),
             filepath = "forex_currencies.csv",
             poke_interval = 5,
             timeout = 20
+         )
+
+         #Python operator example
+         downloading_rates = PythonOperator(
+            task_id = "downloading_rates",
+            python_callable = download_rates
          )
